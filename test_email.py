406c5155822cc4d92a00f13e15e74fd49db0e26b
Chandan Randhawa <zsrandhawa@gmail.com>
2021-12-22 18:17:54 +0530

    last commit

--- test_email.py
+++ test_email.py
@@ -6,7 +6,7 @@ def test_app_status():
     response = client.post("/sendEmail?email=chandanveer.singh@digimantra.com")
     assert response.status_code == 200
 
-def test_app_response():
+def test_app_response():      
     client = TestClient(app)
     response = client.post("/sendEmail?email=chandanveer.singh@digimantra.com")
     assert response.json() == {"message": "email has been sent"}
